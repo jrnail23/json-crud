fdb01f5e10c2c73efc7973b6a024169b7bc21424
James Nail <james.nail@lanetix.com>
2016-12-28 23:46:24 -0500

    TEMP: v2 changes

--- curl/task-add-curl.sh
+++ curl/task-add-curl.sh
@@ -1,2 +1,2 @@
 #!/bin/bash  
-curl -X POST -H "content-type:application/json" -d '{"title":"testing"}' http://localhost:8181/task/
+curl -X POST -H "content-type:application/json" -d '{"title":"Run remote client tests","tags":"test"}' http://localhost:8181/task/
